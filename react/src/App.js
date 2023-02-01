5f7d669553e6412915d7aa94912ddffe969faf3c
so0112 <tkddh0112@gmail.com>
2023-02-01 23:47:39 +0900

    Feat: onClick

--- react/src/App.js
+++ react/src/App.js
@@ -1,13 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let post = "강남 고기 맛집";
+  let logo = "ReactBlog";
+  let [title1, setTitle1] = useState("남자 코트 추천");
+  let [title2, setTitle2] = useState("강남 우동 맛집");
+  let [title3, setTitle3] = useState("파이썬 독학");
+
+  let [like, setlike] = useState(0);
+
+  const onLike = () => {
+    setlike(like + 1);
+  };
+
+  const changeTitle1 = () => {
+    // 원본 데이터를 수정하는 것보다는 복사본 수정이 올바른 방법
+    let copy = [...title1];
+    copy[0] = "여자코트 추천";
+    setTitle1(copy);
+  };
+
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>ReactBlog</h4>
+        <h4>{logo}</h4>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4 onClick={changeTitle1}>
+          {title1} <span onClick={onLike}>👍🏻</span> {like}
+        </h4>
+        <p>2월 1일 발행</p>
+      </div>
+      <hr />
+      <div className="list">
+        <h4>{title2}</h4>
+        <p>2월 1일 발행</p>
+      </div>
+      <hr />
+      <div className="list">
+        <h4>{title3}</h4>
+        <p>2월 1일 발행</p>
+      </div>
+      <hr />
     </div>
   );
 }
