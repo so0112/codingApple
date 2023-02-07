5034fe3e7225e6a5b31922b92eddfa51cad1e838
so0112 <tkddh0112@gmail.com>
2023-02-07 21:46:51 +0900

    Chore: bootstrap 설치

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import shopData from "./data/shopData.json";
 
 function App() {
+  console.log(shopData);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+      <Container>
+        <Row>
+          <Col sm>
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" alt="상품1" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col sm>
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" alt="상품1" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col sm>
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" alt="상품1" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
