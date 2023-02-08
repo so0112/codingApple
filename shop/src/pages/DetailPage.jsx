491176b9efca87239684e874e6bad25dc8fffb4c
so0112 <tkddh0112@gmail.com>
2023-02-09 00:13:20 +0900

    Feat: DetailPage route

--- shop/src/pages/DetailPage.jsx
+++ shop/src/pages/DetailPage.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import shopDatas from "../data/shopDatas.json";
 
 const DetailPage = () => {
+  const { id } = useParams();
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6">
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+          <img src={`https://codingapple1.github.io/shop/shoes${Number(id) + 1}.jpg`} width="100%" />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
+          <h4 className="pt-5">{shopDatas[id].title}</h4>
+          <p>{shopDatas[id].content}</p>
+          <p>{shopDatas[id].price} 원</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
