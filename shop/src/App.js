0498f0ef689c960c57176e14481466d77ce3f59a
so0112 <tkddh0112@gmail.com>
2023-02-08 15:02:47 +0900

    Feat: MainPage

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,13 +1,25 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { Routes, Route, Link } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/detail" element={<div>상세페이지임</div>} />
-      <Route path="/about" element={<div>어바웃페이지임</div>} />
-    </Routes>
+    <>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="">Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="">Home</Nav.Link>
+            <Nav.Link href="detail">Detail</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/detail" element={<div>상세페이지임</div>} />
+        <Route path="/about" element={<div>어바웃페이지임</div>} />
+      </Routes>
+    </>
   );
 }
 
--- shop/src/pages/MainPage.jsx
+++ shop/src/pages/MainPage.jsx
@@ -8,15 +8,6 @@ import ShopItem from "../components/ShopItem";
 const MainPage = () => {
   return (
     <div className="App">
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">Shop</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
       <div className="main-bg"></div>
       <Container>
         <Row>
