45db05fcc61220442f7dbd413ec13efb6eaa2286
so0112 <tkddh0112@gmail.com>
2023-02-09 00:14:27 +0900

    Feat: title 사전순 오름차순 button 추가

--- shop/src/pages/MainPage.jsx
+++ shop/src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,12 +6,31 @@ import shopDatas from "../data/shopDatas.json";
 import ShopItem from "../components/ShopItem";
 
 const MainPage = () => {
+  const [shopDatasState, setShopDatasState] = useState([...shopDatas]);
+
+  //TODO: 함수 관심사 분리하기
+  const onSortShopItme = () => {
+    let copyShopDatasState = [...shopDatasState];
+    copyShopDatasState.sort(function (a, b) {
+      if (a.title[0] > b.title[0]) {
+        return 1;
+      }
+      if (a.title[0] < b.title[0]) {
+        return -1;
+      }
+      return 0;
+    });
+    setShopDatasState(copyShopDatasState);
+    console.log(copyShopDatasState);
+  };
+
   return (
     <div className="App">
       <div className="main-bg"></div>
+      <button onClick={onSortShopItme}>정렬 버튼</button>
       <Container>
         <Row>
-          {shopDatas.map((itemProps) => (
+          {shopDatasState.map((itemProps) => (
             <Col sm key={itemProps.id}>
               <ShopItem itemProps={itemProps} />
             </Col>
