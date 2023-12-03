749000c93bd918286db1aae224555be71208da0e
so0112 <tkddh0112@gmail.com>
2023-12-03 23:56:17 +0900

    비동기 테스트용

---
+++ js/callback/setTimeOut.js
@@ -0,0 +1,22 @@
+// promise 정리용 문서
+const loop = ({ message }) => {
+  console.time(message);
+  let num = 0;
+  for (let i = 0; i <= 2000000000; i++) {
+    num += i;
+  }
+  console.timeEnd(message);
+};
+
+const syncTest = ({ message, time }) => {
+  setTimeout(() => {
+    console.log(message);
+  }, time);
+};
+
+syncTest({ message: '비동기 1번', time: 3000 });
+syncTest({ message: '비동기 2번', time: 1500 });
+syncTest({ message: '비동기 3번', time: 200 });
+
+loop({ message: '동기 4번', loopNum: 2000000000 });
+loop({ message: '동기 5번', loopNum: 500000 });
